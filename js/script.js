f69898e4a3d930ab4d9933de6cfaea849288e646
iwdath <lazorgrechak@gmail.com>
2023-12-13 00:56:20 +0200

    feat(scripts): add posts load
    This commit adds the loading of posts from the array on the first
    login, the loading of posts when the "load more" button is clicked,
    and the formatting of the date.

--- js/script.js
+++ js/script.js
@@ -1,64 +1,96 @@
+// define an array of posts
 const posts = [
-  { number: 0, message: 'test message', date: '1970-01-01T04:15', datetime: 'January 1st, 1970 4:15 AM' }
+  { number: 0, date: "1970-01-01T04:15", message: "test message 0" },
 ];
 
-const postsPerPage = 10;
-let currentPage = 0;
+// define the number of posts displayed
+let displayedPosts = 0;
 
-function createPost(post) {
+// add posts to the site
+function addPosts(count) {
+  const container = document.querySelector('.main');
+
+  // loop through the posts to add
+  for (let i = displayedPosts; i < displayedPosts + count; i++) {
+    if (i >= posts.length) break;
+    const post = posts[i];
+
+    // create the post structure
+    const article = createPostStructure(post);
+    container.appendChild(article);
+  }
+
+  displayedPosts += count;
+
+  if (displayedPosts >= posts.length) {
+    document.querySelector('.load-btn').remove();
+  }
+}
+
+function createPostStructure(post) {
   const article = document.createElement('article');
-  article.classList.add('post');
+  article.className = 'post';
 
-  const info = document.createElement('div');
-  info.classList.add('post__info');
+  const postInfo = document.createElement('div');
+  postInfo.className = 'post__info';
 
-  const date = document.createElement('time');
-  date.classList.add('post__date');
-  date.setAttribute('datetime', post.date);
-  date.textContent = post.datetime;
+  const postDate = createPostDateElement(post.date);
+  const postNumber = createPostNumberElement(post.number);
 
-  const number = document.createElement('p');
-  number.classList.add('post__number');
-  number.textContent = 'post #' + post.number;
+  const postMessage = createPostMessageElement(post.message);
 
-  const message = document.createElement('div');
-  message.classList.add('post__message');
-
-  const p = document.createElement('p');
-  p.textContent = post.message;
-
-  message.appendChild(p);
-  info.appendChild(date);
-  info.appendChild(number);
-  article.appendChild(info);
-  article.appendChild(message);
+  postInfo.appendChild(postDate);
+  postInfo.appendChild(postNumber);
+  article.appendChild(postInfo);
+  article.appendChild(postMessage);
 
   return article;
 }
 
-function displayPosts() {
-  const start = currentPage * postsPerPage;
-  const end = start + postsPerPage;
-  const postsToDisplay = posts.slice(start, end);
-
-  const postsDiv = document.getElementById('posts');
-  postsDiv.innerHTML = '';
-
-  postsToDisplay.forEach(post => {
-    const postElement = createPost(post);
-    postsDiv.appendChild(postElement);
-  });
-
-  if (start > 0) {
-    document.getElementById('load-more').style.display = 'block';
-  } else {
-    document.getElementById('load-more').style.display = 'none';
-  }
+function createPostDateElement(date) {
+  const postDate = document.createElement('time');
+  postDate.className = 'post__date';
+  postDate.setAttribute('datetime', date);
+  postDate.textContent = formatDate(date);
+  return postDate;
 }
 
-document.getElementById('load-more').addEventListener('click', () => {
-  currentPage++;
-  displayPosts();
+function createPostNumberElement(number) {
+  const postNumber = document.createElement('p');
+  postNumber.className = 'post__number';
+  postNumber.textContent = 'post #' + number;
+  return postNumber;
+}
+
+function createPostMessageElement(message) {
+  const postMessage = document.createElement('div');
+  postMessage.className = 'post__message';
+
+  const p = document.createElement('p');
+  p.textContent = message;
+  postMessage.appendChild(p);
+
+  return postMessage;
+}
+
+// format the date
+function formatDate(dateString) {
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  const [date, time] = dateString.split('T');
+  const [year, month, day] = date.split('-');
+  const [hour, minute] = time.split(':');
+
+  const formattedDate = `${months[parseInt(month) - 1]} ${parseInt(day)}, ${year} ${hour}:${minute} ${hour < 12 ? 'AM' : 'PM'}`;
+  return formattedDate;
+}
+
+// load more posts when the button is clicked
+document.querySelector('.load-btn').addEventListener('click', () => {
+  addPosts(10);
 });
 
-displayPosts();
+// load posts when the site is first loaded
+document.addEventListener('DOMContentLoaded', () => {
+  addPosts(10);
+});
