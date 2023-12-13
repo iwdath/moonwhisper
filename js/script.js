23e5a3fcba338b9487247abdce0ee9ffab172813
iwdath <lazorgrechak@gmail.com>
2023-12-13 20:46:12 +0200

    style(scripts): added semicolons and separated logical blocks
    This commit adds a semicolon at the end of functions and adds empty
    lines between logical blocks of code.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // define an array of posts
 const posts = [
-  { number: 0, date: "1970-01-01T04:15", message: "test message 0" },
+  { number: 0, date: "1970-01-01T04:15", message: "test message 0" }
 ];
 
 // define the number of posts displayed
@@ -18,14 +18,14 @@ function addPosts(count) {
     // create the post structure
     const article = createPostStructure(post);
     container.appendChild(article);
-  }
+  };
 
   displayedPosts += count;
 
   if (displayedPosts >= posts.length) {
     document.querySelector('.load-btn').remove();
-  }
-}
+  };
+};
 
 function createPostStructure(post) {
   const article = document.createElement('article');
@@ -45,22 +45,26 @@ function createPostStructure(post) {
   article.appendChild(postMessage);
 
   return article;
-}
+};
 
 function createPostDateElement(date) {
   const postDate = document.createElement('time');
   postDate.className = 'post__date';
+
   postDate.setAttribute('datetime', date);
   postDate.textContent = formatDate(date);
+
   return postDate;
-}
+};
 
 function createPostNumberElement(number) {
   const postNumber = document.createElement('p');
   postNumber.className = 'post__number';
+
   postNumber.textContent = 'post #' + number;
+
   return postNumber;
-}
+};
 
 function createPostMessageElement(message) {
   const postMessage = document.createElement('div');
@@ -71,7 +75,7 @@ function createPostMessageElement(message) {
   postMessage.appendChild(p);
 
   return postMessage;
-}
+};
 
 // format the date
 function formatDate(dateString) {
@@ -82,8 +86,9 @@ function formatDate(dateString) {
   const [hour, minute] = time.split(':');
 
   const formattedDate = `${months[parseInt(month) - 1]} ${parseInt(day)}, ${year} ${hour}:${minute} ${hour < 12 ? 'AM' : 'PM'}`;
+
   return formattedDate;
-}
+};
 
 // load more posts when the button is clicked
 document.querySelector('.load-btn').addEventListener('click', () => {
